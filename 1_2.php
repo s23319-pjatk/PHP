3a1ae48470e29381b66720170cc40357bc1c9728
s23319 <s23319.pjat.edu.pl>
2022-03-13 16:12:34 +0100

    zadanka 2-4

---
+++ 1_2.php
@@ -0,0 +1,9 @@
+<?php
+function rect_area($lenght = 14, $width = 12)
+{
+    $area =$lenght * $width;
+    echo "Pole prostokąta o długości "
+        . $lenght . " i wysokości " . $width . " wynosi " . $area ;
+}
+rect_area();
+?>
---
+++ 1_4.php
@@ -0,0 +1,11 @@
+<?php
+    $a=5;
+    $b=9;
+    $c=$a+$b;
+    $d=$a-$b;
+    $e=$a*$b;
+    $f=$a/$b;
+        echo "wynik dodawania to ", $c=$a+$b;
+        echo "wynik odejmowania to ",
+
+?>
