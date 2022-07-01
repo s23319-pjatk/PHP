cca77d71a12b80badf505de037ad6098826d7804
s23319-pjatk <73029837+s23319-pjatk@users.noreply.github.com>
2022-07-01 22:05:33 +0200

    Update 1_2.php

--- 1_2.php
+++ 1_2.php
@@ -1,9 +1,1 @@
-<?php
-function rect_area($lenght = 14, $width = 12)
-{
-    $area =$lenght * $width;
-    echo "Pole prostokąta o długości "
-        . $lenght . " i wysokości " . $width . " wynosi " . $area ;
-}
-rect_area();
-?>
+
